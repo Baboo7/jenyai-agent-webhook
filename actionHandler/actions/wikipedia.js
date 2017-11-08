e69ecb4816134c406d71177e0ada7710b78226b7
Baboo7 <baptiste.studer@laposte.net>
2017-11-08 00:08:23 -0800

    retrieve description from wikipedia

--- actionHandler/actions/wikipedia.js
+++ actionHandler/actions/wikipedia.js
@@ -24,9 +24,22 @@ const wikipedia = interaction => {
     let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${any}&limit=1&namespace=0&format=json`;
 
     axios.get(url)
-    .then(data => {
-      // #####
-      console.log(data);
+    .then(res => {
+
+      let followupEvent = interaction.followupEvent;
+      let beutifyAny = res.data[1][0];
+      let description = res.data[2][0];
+
+      if(!description ||
+        /may refer to:/.test(description)) {
+        followupEvent.name = 'smalltalk_do_you_know_who_is-descr_not_found';
+        followupEvent.data.any = any;
+      } else {
+        followupEvent.name = 'smalltalk_do_you_know_who_is-descr_found';
+        followupEvent.data.any = beutifyAny;
+        followupEvent.data.description = description;
+      }
+
       resolve();
     })
     .catch(err => {
