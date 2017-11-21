b9ee42d297d1d5cc283cdb11d16284a3dc7c954d
Baboo7 <baptiste.studer@laposte.net>
2017-11-20 17:10:19 -0800

    integrate skills route

--- router/index.js
+++ router/index.js
@@ -2,8 +2,10 @@
 
 let router = require('express').Router();
 
+const skills = require('./skills');
 const webhook = require('./webhook');
 
+router.use('/skills', skills);
 router.post('/', webhook);
 
 module.exports = router;
