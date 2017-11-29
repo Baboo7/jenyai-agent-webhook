3299fefe34cad650cd71390ca9762e423d30b998
Baboo7 <baptiste.studer@laposte.net>
2017-11-29 13:44:28 -0800

    integrate getCard action handler

--- actionHandler/actions/index.js
+++ actionHandler/actions/index.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const getCard = require('./getCard');
 const wikipedia = require('./wikipedia');
 
+/*  In this file are listed all the handled actions and the functions associated.
+
+*/
 const actions = {
+  getCard: getCard,
   wikipedia: wikipedia
 };
 
