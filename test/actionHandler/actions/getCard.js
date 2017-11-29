2c090c907e05002a56e5caaa92e078e7b6e2fea1
Baboo7 <baptiste.studer@laposte.net>
2017-11-29 13:43:59 -0800

    create handler for getCard action

---
+++ test/actionHandler/actions/getCard.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let handler = require('../../../actionHandler/actions/getCard');
+let Interaction = require('../../../utils/interaction');
+let expect = require('chai').expect;
+
+describe('getCard handler', () => {
+
+  it('> get a card for a specific skill', done => {
+
+    let interaction = new Interaction({
+      action: 'getCard',
+      parameters: {
+        skill: '6.R.1'
+      },
+			messages: [
+				{
+					type: 4,
+					payload: {
+						placeholder: 'card'
+					}
+				}
+			]
+    });
+
+		try {
+	    handler(interaction)
+	    .then(() => {
+	      expect(true).to.equal(true);
+	      done();
+	  	})
+	    .catch(e => {
+	      console.log(e);
+	      expect(true).to.equal(false);
+	      done();
+	    });
+		} catch (e) {
+			console.log(e);
+			expect(true).to.equal(false);
+			done();
+		}
+  });
+
+});
